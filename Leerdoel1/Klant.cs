697bdf46cbe521936b8f0015b5ec71eb1ae0f013
pceh <33062910+pceh@users.noreply.github.com>
2019-04-01 00:04:58 +0200

    Add project files.

---
+++ Leerdoel1/Klant.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leerdoel1
+{
+    class Klant
+    {
+        private string naam;
+        private int affiliate_aantal;
+        private decimal pasbedrag;
+        public int user_status; //Moet toegangkelijk zijn voor de switch cases
+
+        public string Naam
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+        public int Affiliate_aantal
+        {
+            get { return affiliate_aantal; }
+            set { affiliate_aantal = value; }
+        }
+
+        public decimal Pasbedrag
+        {
+            get { return pasbedrag; }
+            set { pasbedrag = value; }
+        }
+
+        public Klant()
+        {
+            //Lege ctor voor vroegtijdige initialisatie
+        }
+
+        public Klant(string _naam, int _affiliate_aantal, decimal _pasbedrag)
+        {
+            this.affiliate_aantal = _affiliate_aantal;
+            this.naam = _naam;
+            this.pasbedrag = _pasbedrag;
+        }
+
+
+        public override string ToString()
+        {
+            switch (user_status)
+            {
+                case 1:
+                    return affiliate_aantal.ToString();
+                case 2:
+                    return "Naam: " + Naam + "    Aantal: " + affiliate_aantal;
+                case 3:
+                    return "Naam: " + Naam + "    Saldo: " + pasbedrag;
+                case 4:
+                    return Naam;
+                default:
+                    return "Error: Geef een user_status aan";
+
+            }
+            //  if (user_status == 1)
+            //  {
+            //      return affiliate_aantal.ToString();
+            //  }
+            //  else
+            //  {
+            //      return "Naam: " + Naam + "    Aantal: " + affiliate_aantal;
+            //  }
+            //
+            //  Ik kon geen elseif gebruiken?
+
+
+        }
+
+    }
+}
---
+++ Leerdoel1/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leerdoel1
+{
+    class Product
+    {
+        private string naam;
+        private double prijs;
+        public string Naam { get; set; }
+        public double Prijs { get; set; }
+        public decimal bedrag;
+        private decimal discount;
+        decimal[] kosten = new decimal[] {2.50M, 2.75M, 3, 2, 1.50M, 2.20M, 7.50M, 8, 9 };
+
+
+        public Product(string _naam, double _prijs)
+        {
+            this.naam = _naam;
+            this.prijs = _prijs;
+        }
+
+        public Product()
+        {
+        }
+
+        public decimal korting(decimal bedrag, int aanbreng)
+        {
+            //Aanbreng algoritme
+            decimal huidigBedrag = bedrag;
+            for (int i = 0; i < 10; i++) //for-loop voor aantal aangebrachten leden voor geselecteerde klant 'aanbreng' 
+            {
+                
+                bedrag *= 0.99M;
+                decimal bespaard = huidigBedrag - discount; //Hoeveel korting per iteratie
+                decimal relatief = (bedrag - huidigBedrag) / huidigBedrag * 100;
+                discount = bedrag;
+            }
+            return discount;
+        }
+
+        public decimal Totaal(int counter1, int counter2, int counter3, int counter4, int counter5, int counter6, int counter7, int counter8, int counter9)
+        {
+            //Afzet algoritme
+            bedrag = counter1 * kosten[0] + counter2 * kosten[1] + counter3 * kosten[2] + counter4 * kosten[3] + counter5 * kosten[4] + counter6 * kosten[5] + counter7 * kosten[6] + counter8 * kosten[7] + counter9 * kosten[8];
+
+            return bedrag;
+        }
+
+        public override string ToString()
+        {
+            return naam + prijs;
+        }
+    }
+}
